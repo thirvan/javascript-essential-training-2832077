339ee5e9741f1e483cdd3f01ce20e96407f00eed
thirvan <thirvansomanah@gmail.com>
2022-01-24 11:36:59 +0400

    add practice objects

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,28 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const student = {
+    name: "Thirvan Somanah",
+    age: 20
+}
+
+const laptop = {
+    brand: "Dell",
+    model: "XPS 13 9300",
+    cpu: "i7",
+    storageCapacity: "1 TB",
+    memory: "16 GB",
+    ports: {
+        USBTypeA: 0,
+        USBTypeB: 0,
+        USBTypeC: 2,
+        ethernetPort: 0,
+        sdCardReader: 0,
+        microSdCardReader: 1,
+        HDMIPort: 0,
+        displayPort: 0,
+        others: null
+    },
+    fingerPrintSensor: true
+}
