1ced1aa6019a26f343b54bcce4f064fcd242dfd4
thirvan <thirvansomanah@gmail.com>
2022-01-24 11:58:32 +0400

    complete method practice

--- Practice/03_09/script.js
+++ Practice/03_09/script.js
@@ -23,4 +23,16 @@ const backpack = {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   },
+  changeName: function (newName) {
+    this.name = newName;
+  },
+  changeVolume: function (newVolume) {
+    this.volume = newVolume;
+  },
+  changeColor: function (newColor) {
+    this.color= newColor;
+  },
+  changePocketNum: function (newPocketNum) {
+    this.pocketNum= newPocketNum;
+  }
 };
