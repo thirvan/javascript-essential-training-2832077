93b0ab771c0ac6dc7b8dc97da89b1a723b078c99
thirvan <thirvansomanah@gmail.com>
2022-01-25 09:25:37 +0400

    create first book object

--- 03_14/script.js
+++ 03_14/script.js
@@ -6,6 +6,7 @@
  */
 
 import Backpack from "./Backpack.js";
+import Book from "./Book.js";
 
 const everydayPack = new Backpack(
   "Everyday Backpack",
@@ -18,6 +19,17 @@ const everydayPack = new Backpack(
   "December 5, 2018 15:00:00 PST"
 );
 
-console.log("The everydayPack object:", everydayPack);
-console.log("The pocketNum value:", everydayPack.pocketNum);
-console.log("Days since aquired:", everydayPack.backpackAge());
+// console.log("The everydayPack object:", everydayPack);
+// console.log("The pocketNum value:", everydayPack.pocketNum);
+// console.log("Days since aquired:", everydayPack.backpackAge());
+
+const firstBook = new Book(
+  12345678,
+  "Harry Potter and the Philosopher's Stone",
+  "J.K. Rowling",
+  "4 November 2001"
+);
+
+console.log("The firstBook object:", firstBook);
+console.log("The book was published on:", firstBook.publishedDate);
+console.log("The book is", firstBook.bookAge(), "days old");
