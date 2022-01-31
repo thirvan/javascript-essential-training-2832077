73e74d8dd57f663111c8f9714d04b932ba12a365
thirvan <thirvansomanah@gmail.com>
2022-01-31 10:34:07 +0400

    complete challenge

--- 07_04/script.js
+++ 07_04/script.js
@@ -9,3 +9,41 @@
  */
 
 
+let table = [
+    "laptop",
+    "mouse",
+    "bottle",
+    "keyboard",
+    "monitor",
+    "pen",
+    "paper",
+    "watch",
+];
+console.log( "Initialised array:", table);
+
+let last = table.pop();
+console.log("Removed last item:", last);
+
+console.log("Array after pop:", table);
+
+table.unshift(last);
+console.log("Add last item as first item");
+console.log("Array after adding last item first:", table);
+
+
+table.sort();
+console.log("Sort items");
+console.log("Table array:", table);
+
+let searchItem = "bottl";
+let found = table.find(item => item === searchItem);
+if (found) {
+    console.log(`"${searchItem}", item found`);
+}
+else {
+    console.log(`"${searchItem}", item not found`);
+}
+
+searchItem = "monitor";
+table.splice(table.indexOf(searchItem), 1)
+console.log(table);
