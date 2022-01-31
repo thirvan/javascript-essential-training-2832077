cc038ec22635f4be7515c67ffd5cdfd42314dbaf
thirvan <thirvansomanah@gmail.com>
2022-01-31 10:33:17 +0400

    complete practice

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,28 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+
+const navlist = document.querySelector("main nav ul");
+const myLi = document.querySelector("#test");
+
+function elementInfo(element) {
+    console.log(`element ${element.localName} has id: '${element.id}' and classes: '${element.className}'`);
+}
+
+const makeRed = function(element) {
+    element.classList.add("red");
+}
+
+const makeGreen = (element) => {
+    element.classList.add("green");
+}
+
+elementInfo(navlist);
+elementInfo(myLi);
+
+makeGreen(navlist);
+makeRed(myLi);
+
+elementInfo(navlist);
+elementInfo(myLi);
